00505f234557eff72cc6574d77742d5c21368b43
darenjacobs <darenjacobs@msn.com>
2018-03-20 09:36:22 -0400

    corrections

--- dm.sh
+++ dm.sh
@@ -13,13 +13,14 @@ else
   exit 0
 fi
 
-JENKINS_PORT=8082
-VIZ_PORT=8083
+JENKINS_PORT=8080
+VIZ_PORT=80
 SWARM_PORT=2377
 basename=aws-
 swarm_manager=""
 nodes=3
 
+# Create docker cluster, set first one as manager
 for (( i = 0; i < nodes; i++ ));
 do
   docker-machine -D create --driver amazonec2 \
@@ -37,7 +38,7 @@ do
   fi
 done
 
-# Create directories
+# Create Docker directories on nodes, src is not my local machine, but the docker-machine
 for (( i = 0; i < nodes; i++ ));
 do
   eval $(docker-machine env ${basename}${i})
@@ -84,7 +85,7 @@ eval $(docker-machine env $swarm_manager)
 docker service create \
   --name registry \
   -p 5000:5000 \
-  --mount "type=bind,source=/docker,target=/var/lib/registry" \
+  --mount "type=bind,src=/docker,dst=/var/lib/registry" \
   --reserve-memory 100m registry
 
 docker service ps registry
@@ -101,7 +102,7 @@ docker service create \
 docker service ps jenkins
 
 
-#Docker admin password
+# Get Docker admin password && make Docker fault tolerant
 eval $(docker-machine env $swarm_manager)
 sleep 20
 NODE=$(docker service ps -f desired-state=running jenkins | tail -1 | awk '{print $4}')
@@ -114,14 +115,13 @@ docker-machine ls
 docker service ps jenkins
 
 
-#Jenkins Agents
-
+# Jenkins Agent
 export USER=admin && export PASSWORD=$secret
 docker service create \
   --name jenkins-agent \
   -e COMMAND_OPTIONS="master http://$(docker-machine ip $swarm_manager):$JENKINS_PORT \
   -username $USER -password $PASSWORD -labels 'docker' -executors 2" \
   --mount "type=bind,src=/var/run/docker.sock,dst=/var/run/docker.sock" \
-  --mount "type=bind,src=/workspace,dst=/workspace" \
+  --mount "type=bind,src=/docker/workspace,dst=/workspace" \
   --mount "type=bind,src=/docker/machines,target=/machines" \
   --mode global vfarcic/jenkins-swarm-agent
